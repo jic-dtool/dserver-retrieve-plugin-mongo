98f841af10c1184c67e5591f837b5582e19b5c35
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2023-02-03 15:57:51 +0100

    ENH: secrets to obfuscate implemented

--- dtool_lookup_server_retrieve_plugin_mongo/config.py
+++ dtool_lookup_server_retrieve_plugin_mongo/config.py
@@ -1,23 +1,12 @@
-import json
 import os
 
-import dtool_lookup_server_retrieve_plugin_mongo
-
+CONFIG_SECRETS_TO_OBFUSCATE = [
+    "RETRIEVE_MONGO_URI",
+    "RETRIEVE_MONGO_DB",
+    "RETRIEVE_MONGO_COLLECTION",
+]
 
 class Config(object):
     RETRIEVE_MONGO_URI = os.environ.get("RETRIEVE_MONGO_URI", "mongodb://localhost:27017/")
     RETRIEVE_MONGO_DB = os.environ.get("RETRIEVE_MONGO_DB", "dtool_info")
     RETRIEVE_MONGO_COLLECTION = os.environ.get("RETRIEVE_MONGO_COLLECTION", "datasets")
-
-    @classmethod
-    def to_dict(cls):
-        """Convert server configuration into dict."""
-        exclusions = [
-            # sensitive data, i.e. password
-        ]  # config keys to exclude
-        d = {"version": dtool_lookup_server_retrieve_plugin_mongo.__version__}
-        for k, v in cls.__dict__.items():
-            # select only capitalized fields
-            if k.upper() == k and k not in exclusions:
-                d[k.lower()] = v
-        return d
