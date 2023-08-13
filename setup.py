efef2506af7aa329e75e5f574cf4833ba1446cf7
Tjelvar Olsson <tjelvar.olsson@gmail.com>
2023-08-13 06:28:06 +0100

    Update version number to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 url = "https://github.com/jic-dtool/dtool-lookup-server-retrieve-plugin-mongo"
-version = "0.1.0"
+version = "0.2.0"
 readme = open('README.rst').read()
 
 setup(
